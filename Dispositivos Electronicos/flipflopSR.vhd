048da56730747daa328a040ce97fe8e175155476
Stiven Cardona Monsalve <cardona-627@utp.edu.co>
2017-03-09 21:34:59 -0500

    Update flipflopSR.vhd

--- Dispositivos Electronicos/flipflopSR.vhd
+++ Dispositivos Electronicos/flipflopSR.vhd
@@ -1,34 +1,13 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- University: Universidad Tecnológica de Pereira
+-- Engineer: Stiven Cardona Monsalve
 -- 
 -- Create Date:    14:40:44 03/09/2017 
--- Design Name: 
 -- Module Name:    flipflopSR - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity flipflopSR is
     Port ( CLK : in  STD_LOGIC;
